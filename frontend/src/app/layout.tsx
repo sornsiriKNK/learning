5c1e11ebb768502b73f99a7b75d6b0d4ebccba96
sornsiriKNK <sornsiri.ka@digital.bluebik.com>
2026-05-27 18:11:34 +0700

    clean

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
-import { AppShell } from "@/components/AppShell";
 import { Providers } from "./providers";
 import "./globals.css";
+import Sidebar from "./layouts/sidebar";
 
 
 const geistSans = Geist({
@@ -30,9 +30,8 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>
         <AppRouterCacheProvider>
-          <Providers>
-            <AppShell>{children}</AppShell>
-          </Providers>
+          <Sidebar/>
+          <Providers>{children}</Providers>
         </AppRouterCacheProvider>
       </body>
     </html>
